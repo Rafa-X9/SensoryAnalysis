dbc6f9d677b611a806188fbcc7085506ecd8770e
RafaX9 <rafaeldealencarfloriano@gmail.com>
2026-05-26 13:41:48 -0400

    Initial commit

---
+++ Contracts/ITestManagerService.cs
@@ -0,0 +1,10 @@
+﻿namespace SensoryAnalysis.Contracts;
+/// <summary>
+/// Interface to make the general test services.
+/// This interface will handle the CRUD operations of tests,
+/// passing them to the appropriate service
+/// </summary>
+public interface ITestManagerService
+{
+
+}
---
+++ SensoryAnalysis.Web/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SensoryAnalysis.Web.Controllers;
+[Route("home")]
+public class HomeController : Controller
+{
+    [Route("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("/")]
+    public IActionResult RedirectToIndex()
+    {
+        return RedirectToAction("Index");
+    }
+}
